fe274f1d7c8dffbd38d6be16d1744bda3a69c22c
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-08-08 09:04:27 -0400

    evict deleted item from cache

--- sick-fits/frontend/components/RemoveFromCart.js
+++ sick-fits/frontend/components/RemoveFromCart.js
@@ -19,10 +19,21 @@ const REMOVE_FROM_CART_MUTATION = gql`
         }
     }
 `
+// evict deleted item from the cache:
+function update(cache, payload) {
+    cache.evict(cache.identify(payload.data.deleteCartItem))
+}
 
 export default function RemoveFromCart({ id }) {
     const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
         variables: { id },
+        update,
+        // optimisticResponse: {
+        //     deleteCartItem: {
+        //         __typename: 'CartItem',
+        //         id,
+        //     },
+        // },
     });
     return (
     <BigButton
