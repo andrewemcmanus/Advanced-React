9138e037bc206c0d9d0c52ba617a25357c1d2c29
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-08-31 09:24:00 -0400

    gated signin component

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,11 +1,16 @@
 // import Page from '../components/Page';
 // No need to wrap everything in Page tag because of _app.js
 import CreateProduct from "../components/CreateProduct";
+import PleaseSignIn from "../components/PleaseSignIn";
 
+// Gated PleaseSignIn component: prevents components from displaying
+// "return children" allows this
 export default function SellPage() {
     return (
             <div>
-                <CreateProduct />
+                <PleaseSignIn>
+                    <CreateProduct />
+                </PleaseSignIn>
             </div>
         )
 }
